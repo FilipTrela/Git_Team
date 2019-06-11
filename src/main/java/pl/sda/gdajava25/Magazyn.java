ee5e0aa7c8d9f00bd823de31d52c8b476c8e6c32
Filip Trela <ftrela@gmail.com>
2019-06-11 19:47:21 +0200

    Naprawa

--- src/main/java/pl/sda/gdajava25/Magazyn.java
+++ src/main/java/pl/sda/gdajava25/Magazyn.java
@@ -1,6 +1,13 @@
 package pl.sda.gdajava25;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Map;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class Magazyn {
     Map<String,Produkt> produktMap;
